81f659a1439786f0926021aa0b35afa859d60cb5
Горин Пётр <peter14102005@gmail.com>
2024-04-26 13:55:30 +0300

    That step was easy as pie.

--- src/genDiff.js
+++ src/genDiff.js
@@ -3,8 +3,11 @@ const program = new Command();
 
 program
   .name('gendiff')
-  .description(' Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference.')
   .version('0.8.0');
 program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information');
 program.parse();
