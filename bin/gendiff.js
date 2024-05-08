ecc12b02eb373dec477599c470188fa7b3caade6
Горин Пётр <peter14102005@gmail.com>
2024-05-08 14:19:01 +0300

    Step 4, not quite working as planned, but I'll find a way.

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,20 +2,16 @@
 
 import { Command } from "commander";
 import genDiff from '../src/index.js';
-import { getPath, getData } from '../src/parseJson.js';
 const program = new Command();
 
 program
     .name('gendiff')
     .version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
-    .argument('<filepat1>', 'path to file1')
-    .argument('<filepath2>', 'path to file2')
+    .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format')
-    .action((filepath1, filepath2, format) => {
+    .action((filepath1, filepath2) => {
         console.log(genDiff(filepath1, filepath2)); 
     })
 
-// program.help();
-
-// program.action();
+    program.parse(process.argv);
