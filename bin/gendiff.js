0c427bf1cab8e969f03af13e74f725f361223ba5
Горин Пётр <peter14102005@gmail.com>
2024-05-13 12:05:10 +0300

    Minor fixes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
+import { Command } from 'commander';
 import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
-    .name('gendiff')
-    .version('0.1.0')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format <type>', 'output format')
-    .action((filepath1, filepath2) => {
-        console.log(genDiff(filepath1, filepath2)); 
-    })
+  .name('gendiff')
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
-    program.parse(process.argv);
+program.parse(process.argv);
--- src/parseJson.js
+++ src/parseJson.js
@@ -2,14 +2,14 @@ import fs from 'fs';
 import path from 'path';
 
 function getData(str) {
-    let info = {};
-    if (str.startsWith('/')) {
-      info = path.resolve(str);
-    } else {
-      const directoryName = process.cwd(str);
-      info = path.resolve(directoryName, str);
-    }
-    return JSON.parse(fs.readFileSync(info));
+  let info = {};
+  if (str.startsWith('/')) {
+    info = path.resolve(str);
+  } else {
+    const directoryName = process.cwd(str);
+    info = path.resolve(directoryName, str);
   }
+  return JSON.parse(fs.readFileSync(info));
+}
 
 export default getData;
